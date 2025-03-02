a82a83244efd54fed039a893775512f9a8356e01
akapu <alexkap544@gmail.com>
2025-03-03 00:41:23 +0300

    remove value from text-field

--- src/components/text-field.js
+++ src/components/text-field.js
@@ -3,24 +3,18 @@ import { LitElement, html } from "lit-element";
 class TextField extends LitElement {
   static properties = {
     disabled: { type: Boolean },
-    value: {
-      type: String,
-      reflect: true,
-    },
   };
 
   constructor() {
     super();
-    this.value = "";
     this.disabled = false;
   }
 
   changeValue(e) {
     const newValue = e.target.value;
-    this.value = newValue;
 
     const changeEvent = new CustomEvent("value-changed", {
-      detail: { value: this.value },
+      detail: { value: newValue},
       bubbles: true,
       composed: true,
     });
