d77df366b674d68b96998f4614304b0aef27c9f2
akapu <alexkap544@gmail.com>
2025-03-02 18:08:44 +0300

    add stub FileField component

---
+++ src/components/file-field.js
@@ -0,0 +1,13 @@
+import { LitElement, html, css } from 'lit';
+
+class FileField extends LitElement {
+  static styles = css`
+  `;
+
+  render() {
+    return html`
+    `;
+  }
+}
+
+customElements.define('file-field', FileField);
