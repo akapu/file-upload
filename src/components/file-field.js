0b8e2d5a70d7c418aa92a78768a19e4bc43a084b
akapu <alexkap544@gmail.com>
2025-03-02 20:57:44 +0300

    emit event at file field on file selection

--- src/components/file-field.js
+++ src/components/file-field.js
@@ -2,20 +2,57 @@ import { LitElement, html, css } from "lit";
 
 class FileField extends LitElement {
   static styles = css`
+    .dropzone {
+      cursor: pointer;
+    }
+
     input {
       display: none;
     }
   `;
 
-  handleFileSelect(e) {}
+  handleFileSelect(e) {
+    const file = e.target.files[0];
+    if (file) {
+      this.dispatchFileSelected(file);
+    }
+  }
 
   passClickToInput() {
     this.shadowRoot.querySelector("input").click();
   }
 
+  drop(e) {
+    e.preventDefault();
+
+    const files = e.dataTransfer.files;
+    if (files.length > 0) {
+      this.dispatchFileSelected(files[0]);
+    }
+  }
+
+  dragover(e) {
+    e.preventDefault();
+  }
+
+  dispatchFileSelected(file) {
+    const fileSelected = new CustomEvent("file-selected", {
+      detail: { file },
+      bubbles: true,
+      composed: true,
+    });
+
+    this.dispatchEvent(fileSelected);
+  }
+
   render() {
     return html`
-      <div @click=${this.passClickToInput}>
+      <div
+        class="dropzone"
+        @click=${this.passClickToInput}
+        @drop=${this.drop}
+        @dragover=${this.dragover}
+      >
         <docs-image></docs-image>
       </div>
 
