54f2a81c2bc2c1d0869aadd9ad50ee0c6b784fd9
akapu <alexkap544@gmail.com>
2025-03-02 19:07:24 +0300

    add file input field

--- src/components/file-field.js
+++ src/components/file-field.js
@@ -3,11 +3,18 @@ import DocsImage from '../assets/docs.svg';
 
 class FileField extends LitElement {
   static styles = css`
+  input {
+    display: none;
+  }
   `;
+
+  handleFileSelect(e) {
+  }
 
   render() {
     return html`
     <object type="image/svg+xml" data="${DocsImage}"></object>
+    <input type="file" accept=".txt,.json,.csv" @change=${this.handleFileSelect} />
     `;
   }
 }
