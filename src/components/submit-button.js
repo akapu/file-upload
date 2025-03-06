4177d6d4687b393e2a5b872ef07e1f2b9dcd9b45
akapu <alexkap544@gmail.com>
2025-03-06 14:26:12 +0300

    add styles to submit button

--- src/components/submit-button.js
+++ src/components/submit-button.js
@@ -1,20 +1,56 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
+import { theme } from "../theme.js";
 
 class SubmitButton extends LitElement {
   static styles = css`
+    ${theme.utility.borderBox}
+    ${theme.styles.removeDefaultButton}
+    ${theme.utility.font}
+
+    button {
+      cursor: pointer;
+
+      background: ${theme.colors.primary};
+
+      color: white;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 24px;
+
+      width: 100%;
+      height: 56px;
+      border-radius: 30px;
+
+      padding-top: 16px;
+      padding-right: 86px;
+      padding-bottom: 16px;
+      padding-left: 86px;
+    }
+
+    button:disabled {
+      background: #ccccce47;
+
+      cursor: not-allowed;
+    }
   `;
+
+  static properties = {
+    disabled: { type: Boolean },
+  };
 
   constructor() {
     super();
-  }
 
+    this.disabled = false;
+  }
 
   render() {
     return html`
-    <button>Загрузить</button>
+      <button class="border-box font" ?disabled=${this.disabled}>
+        Загрузить
+      </button>
     `;
   }
 }
 
-customElements.define('submit-button', SubmitButton);
-
+customElements.define("submit-button", SubmitButton);
