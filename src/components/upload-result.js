efd41e77a02fec482cfe43887d01263d08281747
akapu <alexkap544@gmail.com>
2025-03-13 01:50:13 +0300

    add dynamic title

--- src/components/upload-result.js
+++ src/components/upload-result.js
@@ -7,8 +7,18 @@ class UploadResult extends LitElement {
     data: { type: Object },
   };
 
+  get _title() {
+    return this.error ? "Ошибка в загрузке файла" : "Файл успешно загружен";
+  }
+
   render() {
-    return html``;
+    return html`
+      <upload-header>
+        <span slot="title">${this._title}</span>
+
+        <span slot="hint"></span>
+      </upload-header>
+    `;
   }
 }
 
