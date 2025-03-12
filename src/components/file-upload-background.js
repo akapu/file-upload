b6b7095a0af6ecead3ff753b68272117a2b54ed0
akapu <alexkap544@gmail.com>
2025-03-12 21:07:36 +0300

    rework file upload background to use keyframes animation

--- src/components/file-upload-background.js
+++ src/components/file-upload-background.js
@@ -1,9 +1,23 @@
 import { LitElement, css, html } from "lit";
-import { windowBackgrounds } from "../theme";
-import { repeat } from "lit/directives/repeat.js";
-import { styleMap } from "lit/directives/style-map.js";
+import { theme } from "../theme";
+import { KeyframesComposer } from "../KeyframesComposer.js";
+import { createRef, ref } from "lit/directives/ref.js";
 
 export class FileUploadBackground extends LitElement {
+  // Для анимации бэкграундов нужно установить глобальные @property
+
+  // @property --file-upload-background-top {
+  //   syntax: "<color>";
+  //   inherits: true;
+  //   initial-value: transparent;
+  // }
+
+  // @property --file-upload-background-middle {
+  //   syntax: "<color>";
+  //   inherits: true;
+  //   initial-value: transparent;
+  // }
+
   static styles = css`
     :host {
       position: absolute;
@@ -16,37 +30,110 @@ export class FileUploadBackground extends LitElement {
     }
 
     .background {
+      position: absolute;
       width: 100%;
-      height: 100%;
+      height: 479px;
 
-      transition-property: opacity;
-      transition-timing-function: linear;
+      background: linear-gradient(
+        180deg,
+        var(--file-upload-background-top) 0%,
+        var(--file-upload-background-middle) 42.5%,
+        #ffffff 100%
+      );
 
-      opacity: 0;
+      --file-upload-background-top: ${theme.colors.primary};
+      --file-upload-background-middle: #dddcfc;
     }
   `;
 
   static properties = {
-    background: { type: String },
-    duration: { type: Number },
+    state: { type: String },
+    _initial: { type: Boolean, state: true },
   };
 
+  constructor() {
+    super();
+
+    this.state = "form";
+    this._initial = true;
+
+    this.initializeAnimations();
+  }
+
+  initializeAnimations() {
+    this._formToSuccess = new KeyframesComposer(
+      theme.animationDurations.formToResult
+    );
+    this._formToSuccess.setKeyframes([
+      { keyframe: {}, stage: 0 },
+      { keyframe: {}, stage: 1 },
+      { keyframe: {}, stage: 2 },
+      {
+        keyframe: {
+          "--file-upload-background-top": theme.colors.primary,
+          "--file-upload-background-middle": "#dddcfc",
+        },
+        stage: 3,
+      },
+      {
+        keyframe: {
+          "--file-upload-background-top": theme.colors.primary,
+          "--file-upload-background-middle": "#8f8df4",
+        },
+        stage: 4,
+      },
+    ]);
+
+    this._formToError = new KeyframesComposer(
+      theme.animationDurations.formToResult
+    );
+    this._formToError.setKeyframes([
+      { keyframe: {}, stage: 0 },
+      { keyframe: {}, stage: 1 },
+      { keyframe: {}, stage: 2 },
+      {
+        keyframe: {
+          "--file-upload-background-top": "#f05c5c",
+          "--file-upload-background-middle": "#dddcfc",
+        },
+        stage: 3,
+      },
+      {
+        keyframe: {
+          "--file-upload-background-top": "#f05c5c",
+          "--file-upload-background-middle": "#8f8df4",
+        },
+        stage: 4,
+      },
+    ]);
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has("state") && !this._initial) {
+      this.startAnimation();
+    }
+
+    if (this._initial) this._initial = false;
+  }
+
+  startAnimation() {
+    const stateComposerMap = {
+      success: this._formToSuccess,
+      error: this._formToError,
+    };
+
+    const keyframesComposer = stateComposerMap[this.state];
+
+    this._background.value.animate(keyframesComposer.keyframesWithOffsets, {
+      duration: keyframesComposer.totalDuration,
+      fill: "forwards",
+    });
+  }
+
+  _background = createRef();
+
   render() {
-    return html` ${repeat(
-      Object.entries(windowBackgrounds),
-      ([background]) => background,
-      ([background, styles]) =>
-        html`
-          <div
-            class="background"
-            style=${styleMap({
-              ...styles,
-              opacity: this.background === background ? 1 : 0,
-              "transition-duration": this.duration + "ms",
-            })}
-          ></div>
-        `
-    )}`;
+    return html` <div class="background" ${ref(this._background)}></div> `;
   }
 }
 
